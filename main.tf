3f84a3e4afcc48874b8d11cc5a414ef965a52eee
adlambar <48596797+adlambar@users.noreply.github.com>
2022-08-18 11:14:15 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_security_group" "instances" {
+resource "aws_security_group" "sg_general" {
   name = "general"
   ingress {
     from_port   = 22
