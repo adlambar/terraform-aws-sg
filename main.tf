e04e353d16c229b9dca056ac7edd872275a83cc7
adlambar <48596797+adlambar@users.noreply.github.com>
2022-08-18 10:50:24 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_security_group" "instances" {
+  name = "sg-general"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
